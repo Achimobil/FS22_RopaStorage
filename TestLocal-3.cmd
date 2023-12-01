e89b0867c6ac7ac37d3f1c064aa25cc033ad61b1
Achim Kuntz <achimobil@hotmail.com>
2023-12-01 12:19:31 +0100

    change cmd's

--- TestLocal-3.cmd
+++ TestLocal-3.cmd
@@ -1,2 +1,2 @@
-"C:\Program Files\7-Zip\7z.exe" a -tzip "E:\LS22ModTest\mods\FS22_RopaStorage.zip" -x@FileDefinition.txt
-"E:\Program Files (x86)\Farming Simulator 2022\FarmingSimulator2022.exe" -autoStartSavegameId 3 -cheats -profile E:\LS22ModTest
+"C:\Program Files\7-Zip\7z.exe" u -tzip "E:\LS22Profile\LS22ModTest\mods\FS22_RopaStorage.zip" -x@FileDefinition.txt
+start "" "C:\Program Files (x86)\Farming Simulator 2022\x64\FarmingSimulator2022Game.exe" -autoStartSavegameId 3 -cheats -profile E:\LS22Profile\LS22ModTest
